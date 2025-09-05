d9780d800d13ddeafb81079ff4e7e84fc9f35e0e
Christin Koshy <christin.koshy@mandelbulbtech.com>
2025-09-05 14:27:39 +0530

    fix: update PORT in .env and correct server.js path in vercel configuration

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/employees', require('./routes/employees'));
 app.use('/api/attendance', require('./routes/attendance'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
